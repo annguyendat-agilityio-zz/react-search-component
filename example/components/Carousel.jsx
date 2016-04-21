217895ab888adad855056e0521ae02d1b01f5adc
An Nguyen <an.nguyen@asnet.com.vn>
2016-04-21 17:46:15 +0700

    Handle event carousel

--- example/components/Carousel.jsx
+++ example/components/Carousel.jsx
@@ -1,31 +1,34 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 // import Carousel from './test.jsx'
-// // import { Carousel } from '../../lib/component';
+import { Carousel } from '../../lib/component';
 
-// export default class CarouselComponent extends React.Component {
-// 	constructor(props) {
-// 		super(props);
+export default class CarouselComponent extends React.Component {
+	constructor(props) {
+		super(props);
 
-// 	}
+	}
 
-// 	componentDidMount() {
-// 		const base = ReactDOM.findDOMNode(this);
-// 		console.log(base.offsetWidth);
-// 	}
+	componentDidMount() {
+		const base = ReactDOM.findDOMNode(this);
+		console.log(base.offsetWidth);
+	}
 
-// 	render () {
+	render () {
 
-// 		// const { children } = this.prop
+		// const { children } = this.prop
 
-// 		return (
-// 			<div>
-// 				<Carousel>
-// 					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/ambition-morty.png"/>
-// 					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
-// 					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/despise.png"/>
-// 				</Carousel>
-// 			</div>
-// 		)
-// 	}
-// }
+		return (
+			<div>
+				<Carousel>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/ambition-morty.png"/>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/despise.png"/>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/ambition-morty.png"/>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
+					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/despise.png"/>
+				</Carousel>
+			</div>
+		)
+	}
+}
