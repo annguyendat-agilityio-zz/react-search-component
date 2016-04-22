807346b92c03dfef09f27eb89e3035647e789b25
An Nguyen <an.nguyen@asnet.com.vn>
2016-04-22 13:57:41 +0700

    update style

--- example/components/Carousel.jsx
+++ example/components/Carousel.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Carousel from './test.jsx'
 import { Carousel } from '../../lib/component';
 
 export default class CarouselComponent extends React.Component {
@@ -9,19 +8,16 @@ export default class CarouselComponent extends React.Component {
 
 	}
 
-	componentDidMount() {
-		const base = ReactDOM.findDOMNode(this);
-		console.log(base.offsetWidth);
-	}
-
 	render () {
 
 		return (
 			<div>
 				<Carousel>
 					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/ambition-morty.png"/>
-					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
-					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
+					<h1>test 1</h1>
+					<h2>test 2</h2>
+					<h3>test 3</h3>
+					<h4>test 4</h4>
 					<img src="https://raw.githubusercontent.com/jerryshew/design/master/png/awkward-morty.png"/>
 				</Carousel>
 			</div>
